2a6f8e847db903bfbe2777ea4bc7f1f6b2923f5d
GitHubWison <15995448871@163.com>
2017-06-20 14:55:20 +0800

    周报生成，时间分区，新增下周工作计划

--- python_code/weekly_report_builder/main.py
+++ python_code/weekly_report_builder/main.py
@@ -18,7 +18,11 @@ borders_test.right = 1
 borders_test.bottom = 1
 borders_test.top = 1
 
-mission_catagory = ['星期一', '星期二', '星期三', '星期四', '星期五']
+mission_catagory = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+before_days = -10
+
+end_time = datetime.datetime.now() + datetime.timedelta(days=1)
+start_time = end_time + datetime.timedelta(days=before_days)
 
 
 # style = xlwt.XFStyle()
@@ -32,8 +36,6 @@ def export_svn_log(svn_remote_path, svn_username):
     # 切换到本地svn路径去
     # os.chdir(svn_local_path)
     # print os.getcwd()
-    end_time = datetime.datetime.now() + datetime.timedelta(days=1)
-    start_time = end_time + datetime.timedelta(days=-10)
     start_end_time = get_date_range(start_time, end_time)
     print '%s:%s' % ('开始时间', start_end_time[start_time_key])
     print '%s:%s' % ('结束时间', start_end_time[end_time_key])
@@ -61,12 +63,17 @@ def build_excel(list):
 
         # 每天的任务个数
         everyday_mission_len = len(everyday_mission_list)
-        flag_end_index = flag_start_index + everyday_mission_len-1
+        flag_end_index = flag_start_index + everyday_mission_len - 1
         # 合并纵向的单元格
         print '%s===%s===%s' % (flag_start_index, flag_end_index, total_item)
-        ws.write_merge(flag_start_index, flag_end_index, 1, 1, total_item)
-        flag_start_index = flag_end_index+1
-        for item in everyday_mission_list:
+        style_test4 = xlwt.easyxf('font:height 220,name Microsoft YaHei,color_index black,'
+                                  'bold on,italic off;align:wrap on,vert center,horiz center;')
+        style_test4.borders = borders_test
+        ws.write_merge(flag_start_index, flag_end_index, 1, 1, total_item, style_test4)
+        flag_start_index = flag_end_index + 1
+        avg_hour = 8 / everyday_mission_len
+        mod_hour = 8 % everyday_mission_len
+        for every_index, item in enumerate(everyday_mission_list):
             # for index, item in enumerate(list):
             ws.row(index + 2).height = 400
 
@@ -84,6 +91,11 @@ def build_excel(list):
                                       'bold off,italic off;align:wrap on,vert center,horiz left;')  # 填写任务名称
             style_test1.borders = borders_test
             ws.write(index + 2, 3, item, style_test1)
+            # style_test1.alignment.horz = xlwt.Alignment.HORZ_RIGHT
+            if everyday_mission_len - 1 == every_index:
+                ws.write(index + 2, 4, avg_hour + mod_hour, style_test1)
+            else:
+                ws.write(index + 2, 4, avg_hour, style_test1)
 
             style_test = xlwt.easyxf('font:height 220,name Microsoft YaHei,color_index brown,'
                                      'bold on,italic off;align:wrap on,vert center,horiz center;'
@@ -92,38 +104,54 @@ def build_excel(list):
             #     填写完成状况
             ws.write(index + 2, 5, '100%', style_test)
 
+            # 备注
+            ws.write(index + 2, 6, '', style_test1)
             index += 1
+    # 下周工作计划
+    index += 2
+    style_next_week = xlwt.easyxf('font:height 220,name Microsoft YaHei,color_index black,'
+                                  'bold on,italic off;align:wrap on,vert center,horiz center;'
+                                  'pattern: pattern solid,fore_colour gray25;')
+    style_next_week.borders = borders_test
+    ws.row(index).height = 540
+    ws.write_merge(index, index, 1, 5, '下周工作计划', style_next_week)
+    ws.write(index, 0, 'ID', style_next_week)
+    ws.write(index, 6, '计划工时', style_next_week)
+    index += 1
+    style_next_week_content = xlwt.easyxf('font:height 220,name Microsoft YaHei,color_index black,'
+                                          'bold off,italic off;align:wrap on,vert center,horiz center;')
+    style_next_week_content.borders = borders_test
+    ws.write(index, 0, '1', style_next_week_content)
+    ws.write_merge(index, index, 1, 5, '', style_next_week_content)
+    ws.write(index, 6, '', style_next_week_content)
     wb.save('output/test.xls')
 
 
 def init_excel():
-    end_time = datetime.datetime.now() + datetime.timedelta(days=1)
-    start_time = end_time + datetime.timedelta(days=-10)
     start_end_time = get_date_range(start_time, end_time)
     ws.row(0).height = 800
     title_name = '个人工作任务跟踪表(%s至%s)' % (start_end_time[start_time_key], start_end_time[end_time_key])
-    ws.write_merge(0, 0, 0, 6, title_name,
-                   xlwt.easyxf('font:height 360,name Microsoft YaHei,color_index black,'
-                               'bold on,italic off;align:wrap on,vert center,horiz center;'
-                               'pattern: pattern solid,fore_colour light_orange'))
+
+    title_style = xlwt.easyxf('font:height 360,name Microsoft YaHei,color_index black,'
+                              'bold on,italic off;align:wrap on,vert center,horiz center;'
+                              'pattern: pattern solid,fore_colour light_orange')
+    title_style.borders = borders_test
+    ws.write_merge(0, 0, 0, 6, title_name, title_style)
     ws.row(1).height = 915
     ws.col(0).width = 1500
     ws.col(3).width = 20000
     capture_style = ('font:height 220,name Microsoft YaHei,color_index black,'
                      'bold on,italic off;align:wrap on,vert center,horiz center;'
                      'pattern: pattern solid,fore_colour pale_blue')
-    ws.write(1, 0, 'ID', xlwt.easyxf(capture_style))
-    ws.write(1, 1, '时间', xlwt.easyxf(capture_style))
-    ws.write(1, 2, '类型', xlwt.easyxf(capture_style))
-    ws.write(1, 3, '任务名称', xlwt.easyxf(capture_style))
-    ws.write(1, 4, '实际工时', xlwt.easyxf(capture_style))
-    ws.write(1, 5, '完成状态', xlwt.easyxf(capture_style))
-    ws.write(1, 6, '备注', xlwt.easyxf(capture_style))
-    # wb.save('output/col.xls')
-
-    # ws.write_merge(2, 2, 1, 1, "23")
-
-    # ws.write_merge(3, 3, 1, 1, "34")
+    cap_style = xlwt.easyxf(capture_style)
+    cap_style.borders = borders_test
+    ws.write(1, 0, 'ID', cap_style)
+    ws.write(1, 1, '时间', cap_style)
+    ws.write(1, 2, '类型', cap_style)
+    ws.write(1, 3, '任务名称', cap_style)
+    ws.write(1, 4, '实际工时', cap_style)
+    ws.write(1, 5, '完成状态', cap_style)
+    ws.write(1, 6, '备注', cap_style)
 
 
 # 提取log中的信息
@@ -183,9 +211,10 @@ def get_date_range(start_time, end_time):
     return {start_time_key: formated_start_t, end_time_key: formated_end_t}
 
 
-export_svn_log('https://svn.mdsd.cn:7443/svn/PreHospitalEmergency/trunk/AndroidApp/WisdomAidApp/trunk/WisdomAidApp',
-               'CP-xuqiwei')
-# build_excel()
-# get_log()
-# init_excel()
-# div_missions(['11111', '22222', '33333', '44444', '555555', '66666', '7777777'])
+# export_svn_log('https://svn.mdsd.cn:7443/svn/PreHospitalEmergency/trunk/AndroidApp/WisdomAidApp/trunk/WisdomAidApp',
+#                'CP-xuqiwei')
+export_svn_log('https://svn.mdsd.cn:7443/svn/PreHospitalEmergency/trunk/高危筛查/Src',
+               'CP-zhouqing')
+# export_svn_log('https://svn.mdsd.cn:7443/svn/PreHospitalEmergency/trunk',
+#                'CP-xuqiwei')
+
