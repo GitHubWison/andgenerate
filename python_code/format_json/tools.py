98091e23472bdf5b05faa8bc71cd86aee2197809
GitHubWison <15995448871@163.com>
2017-06-21 16:51:30 +0800

    执行ａｐｉ请求，并完成界面

--- python_code/format_json/tools.py
+++ python_code/format_json/tools.py
@@ -2,15 +2,26 @@
 import http_request
 import os
 
+
 # request = http_request.Request('http://172.16.23.14:45200/api/mobilemedicalrecord/getmedicalrecordbypatientid/8ee5daa6-ba26-4d17-83b3-eda99ea443ad?AppId=PreHospitalCare')
-request = http_request.Request('http://172.16.23.14:4500/api/Public/CheckUserIsValidWithCarInfo',
-                               method=http_request.METHOD_POST, params={"AppId": "PreHospitalCare", "LoginName": "pad",
-                                                                        "Password": "202CB962AC59075B964B07152D234B70",
-                                                                        "PlateNumber": "6IuPRSAwQTEyNQ=="}
-                               )
-json_res = http_request.FormateJson(request.request())
-pretty_json = json_res.pretty_json()
-# print pretty_json
-file_object = open('outputs/result.txt', 'w')
-file_object.write(pretty_json)
-os.system('xdg-open outputs/result.txt')
+# request = http_request.Request('http://172.16.23.14:4500/api/Public/CheckUserIsValidWithCarInfo',
+#                                method=http_request.METHOD_POST, params={"AppId": "PreHospitalCare", "LoginName": "pad",
+#                                                                         "Password": "202CB962AC59075B964B07152D234B70",
+#                                                                         "PlateNumber": "6IuPRSAwQTEyNQ=="}
+#                                )
+# json_res = http_request.FormateJson(request.request())
+# pretty_json = json_res.pretty_json()
+# # print pretty_json
+# file_object = open('outputs/result.txt', 'w')
+# file_object.write(pretty_json)
+# os.system('xdg-open outputs/result.txt')
+def get_test(url, method=http_request.METHOD_GET, params={}):
+    request = http_request.Request(url, params=params, method=method)
+    json_res = http_request.FormateJson(request.request()).pretty_json()
+
+    file_object = open('format_json/outputs/result.txt', 'w')
+    file_object.write(json_res)
+    os.system('xdg-open format_json/outputs/result.txt')
+    return json_res
+
+# print get_test('http://www.tngou.net/api/info/classify')
--- python_code/routepg.py
+++ python_code/routepg.py
@@ -1,6 +1,9 @@
+# coding=utf-8
 from flask import Flask
 from flask import render_template
+from flask import request, jsonify
 import os
+from format_json.tools import *
 
 app = Flask(__name__)
 
@@ -10,10 +13,10 @@ def hellopage(username):
     return 'Hello %s!' % username
 
 
-@app.route('/indexserver/<deftwoname>')
-def index(deftwoname):
-    # print os.system('pwd')
-    return render_template('index.html', var_one_value='hello', var_two_value=deftwoname)
+# @app.route('/indexserver/<deftwoname>')
+# def index(deftwoname):
+#     print os.system('pwd')
+# return render_template('index.html', var_one_value='hello', var_two_value=deftwoname)
 
 
 @app.route('/showUserName/<showname>')
@@ -21,10 +24,52 @@ def show_user_name(showname):
     index(' world')
 
 
+# 查询数据库
 @app.route('/search_table')
 def search_table():
-    return index('world')
+    return render_template('search_sqlite/index.html')
+
+
+@app.route('/jstest', methods=['POST'])
+def hello_page():
+    inputname = request.form.get('inputname')
+    return jsonify(inputname=('welcome%s' % inputname))
+
+
+@app.route('/jstestindex/<urlpath>')
+def jstest_index(urlpath):
+    return render_template('jstest.html', urlpath=urlpath)
+
+
+@app.route('/requesturl/<urlpath>')
+def requesturl_index(urlpath='', params=''):
+    return render_template('requesturl/index.html', urlpath=urlpath, params=params)
+
+
+@app.route('/requesturl')
+def requesturl_noparam():
+    return requesturl_index('', '')
+
+
+@app.route('/requesturl_result', methods=['POST'])
+def requesturl_result():
+    url = request.form.get('url')
+    method = request.form.get('method')
+    params = request.form.get('params')
+    get_test(url, method=method, params=params)
+    return requesturl_index(url, str(params))
+
+
+# 生成周报
+@app.route('/weekly')
+def build_weekly():
+    return ''
+
+
+@app.route('/index')
+def homepage():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run(host='172.16.23.5', port=8080)
